e33bc5ad3aeb41f5c6e6667fc4a6de7facfdeb4f
EricTzul <etzul@nyit.edu>
2024-10-18 09:19:11 -0400

    Did the Couse and Department

--- Course.java
+++ Course.java
@@ -9,97 +9,31 @@
 
 
 public class Course{
-    public String courseName;
-    private String courseNumber;
-    public String instructorName;
-    private Student[] listStudents;
+    private Professor instructor;
+    private listCollegeStudent[];
+    private String CourseName;
 
-    public Course(String courseName, String courseNumber, String instructorName, Student[] listStudents) {
-        this.courseName = courseName;
-        this.courseNumber = courseNumber;
-        this.instructorName = instructorName;
-        this.listStudents = new Student[listStudents.length];
-        for (int i = 0; i < listStudents.length; i++) {
-            this.listStudents[i] = new Student(listStudents[i]);
+    public Course(Professor instructor, listCollegeStudents[], String CourseName){
+        this.CourseName = CourseName;
+        this.instructor = instructor;
+        this.listCollegeStudents = new CollegeStudent[listCollegeStudents.length];
+        for(int i = 0; i < listCollegeStudents.length; i++){
+            this.listCollegeStudents[i] = new CollegeStudent(listCollegeStudents[i]);
         }
     }
 
-    public Course(){
-        this.courseName = "Social Studies";
-        this.courseNumber = "MO1";
-        this.instructorName = "Wilson";
-        this.listStudents = new Student[0];
-
+    public void addStudent(CollegeStudent student){
+        student.add(student);
     }
 
+    public String toString(){
+       String s = "Info Shown Below:\n";
+       s += "Course Name: " + this.CourseName + "\n";
+       s += "Instructor: " + this.instructor + "\n";
+       for(CollegeStudent student: students){
+           s += student.toString() + "\n";
+       }
+       return s;
 
-    public String getCourseName(){
-        return courseName;
     }
-    public void setCourseName(String courseName) {
-        this.courseName = courseName;
-    }
-    public String getcourseNumber(){
-        return courseNumber;
-    }
-    public void setCourseNumber(String courseNumber) {
-        this.courseNumber = courseNumber;
-    }
-
-    public String getInstructorName(){
-        return instructorName;
-    }
-    public void setInstructorName(String instructorName) {
-        this.instructorName = instructorName;
-    }
-
-    public Student[] getListStudents() {
-        return listStudents;
-    }
-
-    public void setListStudents(Student[] listStudents) {
-        this.listStudents = new Student[listStudents.length];
-        for (int i = 0; i < listStudents.length; i++) {
-            this.listStudents[i] = new Student(listStudents[i]);
-        }
-    }
-
-    public String toString() {
-        StringBuilder s = new StringBuilder("Course Details:\n");
-        s.append("Course Name: ").append(this.courseName).append("\n");
-        s.append("Course Number: ").append(this.courseNumber).append("\n");
-        s.append("Instructor: ").append(this.instructorName).append("\n");
-
-        s.append("\nEnrolled Students:\n");
-        if (listStudents != null && listStudents.length > 0) {
-            for (Student student : listStudents) {
-                s.append(student.toString()).append("\n");
-            }
-        } else {
-            s.append("No students enrolled.\n");
-        }
-
-        return s.toString();
-    }
-
-    public static void main(String[] args) {
-        Student[] studentsForCS = {
-                new Student("John", "12345", 4.0, 18),
-                new Student("David", "67890", 3.9, 18),
-                new Student("Wayne", "29384", 3.6, 17)
-        };
-
-        Student[] studentsForSocialStudies = {
-                new Student("Henry", "11223", 3.8, 19),
-                new Student("Bob", "44556", 3.7, 20),
-                new Student("Naifry", "77889", 3.5, 18)
-        };
-
-        Course c1 = new Course("Computer Science", "MO1", "Henry", studentsForCS);
-        Course c2 = new Course("Social Studies", "MO2", "Wilson", studentsForSocialStudies);
-
-        System.out.println(c1.toString());
-        System.out.println(c2.toString());
-    }
-
 }
--- Department.java
+++ Department.java
@@ -1,107 +1,40 @@
+
 public class Department {
+    private Professor chair;
+    private listProfessor[];
+    private listCourse[];
 
-    private String deptName;
-    private int num_majors;
-    private Teacher[] listTeachers;
-    private Student[] listStudents;
-
-    public Department(String deptName, int numMajors) {
-        this.deptName = deptName;
-        this.num_majors = numMajors;
-        this.listTeachers = new Teacher[3];
-        this.listStudents = new Student[5];
-
-    }
-
-    public Department() {
-        this.deptName = "";
-        this.num_majors = 0;
-        this.listTeachers = new Teacher[3];
-        this.listStudents = new Student[5];
-    }
-
-    public int getNumMajors() {
-        return num_majors;
-    }
-
-    public void setNum_majors(int NumMajors) {
-        this.num_majors = NumMajors;
-    }
-
-    public String getDeptName() {
-        return deptName;
-    }
-
-    public void setDeptName(String deptName) {
-        this.deptName = deptName;
-    }
-
-    public void addTeacher(Teacher teacher, int index) {
-        if (index >= 0 && index < listTeachers.length) {
-            listTeachers[index] = teacher;
+    public Department(Professor chair, listProfessor[], listCourse) {
+        this.chair = chair;
+        this.listProfessor = new Professor[listProfessor.length];
+        for(int i = 0; i < listProfessor.length; i++){
+            this.listProfessor[i] = new Professor(listProfessor[i]);
+        }
+        this.listCourse = new Course[ListCourse.length];
+        for(int i = 0; i < listCourse.length; i++){
+            this.listCourse[i] = new Professor(listProfessor[i]);
         }
     }
 
-    public void addStudent(Student student, int index) {
-        if (index >= 0 && index < listStudents.length) {
-            listStudents[index] = student;
-        }
+    public void addFaculty(Professor prof){
+        listProfessor.add(prof);
     }
 
-    public String toString() {
-        String s = "Info Show Below: \n";
-        s += "Department Name: "+ this.deptName + "\n";
-        s += "Number of Majors: " + this.num_majors + "\n";
-        for (Teacher teacher : listTeachers) {
-            if (teacher != null) {
-                s += (teacher.toString()) + ("\n");
-            }
-        }
-        s += ("Students:\n");
-        for (Student student : listStudents) {
-            if (student != null) {
-                s += (student.toString()) + ("\n");
-            }
-        }
-        return s.toString();
+    public void addCourse(Course course){
+        listCourse.add(course);
     }
 
-    public static void main(String[] args) {
-        Department csDepartment = new Department("Computer Science", 50);
-        Department mathDepartment = new Department("Mathematics", 30);
-
-        Teacher teacher1 = new Teacher("1456943", 60000, 1);
-        Teacher teacher2 = new Teacher("0934892", 90000, 2);
-        Teacher teacher3 = new Teacher("8934855", 40000, 3);
-
-        Student student1 = new Student("1982384", 3.8, "Freshman");
-        Student student2 = new Student("S002", 3.6, "Sophomore");
-        Student student3 = new Student("S003", 3.9, "Junior");
-        Student student4 = new Student("S004", 2.8, "Senior");
-        Student student5 = new Student("S005", 3.5, "Freshman");
-
-        csDepartment.addTeacher(teacher1, 0);
-        csDepartment.addTeacher(teacher2, 1);
-        csDepartment.addTeacher(teacher3, 2);
-
-        csDepartment.addStudent(student1, 0);
-        csDepartment.addStudent(student2, 1);
-        csDepartment.addStudent(student3, 2);
-        csDepartment.addStudent(student4, 3);
-        csDepartment.addStudent(student5, 5);
-
-        mathDepartment.addTeacher(teacher1, 0);
-        mathDepartment.addTeacher(teacher2, 1);
-        mathDepartment.addTeacher(teacher3, 2);
-
-        mathDepartment.addStudent(student1, 0);
-        mathDepartment.addStudent(student2, 1);
-        mathDepartment.addStudent(student3, 2);
-        mathDepartment.addStudent(student4, 3);
-        mathDepartment.addStudent(student5, 4);
-
-
-        System.out.println(csDepartment);
-        System.out.println(mathDepartment);
+    public String toString(){
+        String s = "Info Shown Below\n";
+        s += "Department Chair: " + listProfessor + "\nFaculty: ";
+        for(Professor prof : listProfessor){
+            s += prof.toString() + "\n";
+        }
+        s  += "Courses: ";
+        for(Course course : listCourse){
+            return += course.toString() + "\n";
+        }
+        return s;
     }
 }
+
